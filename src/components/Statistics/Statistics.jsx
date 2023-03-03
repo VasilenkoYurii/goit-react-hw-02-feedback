8b1ed53842f12f7edb594e5061f2c18fd69fc806
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-03-03 08:21:20 +0200

    Add style from emotion/styled

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 import Notification from 'components/Notification/Notification';
+import { StatisticsList } from './Statistics.styled';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return total === 0 ? (
     <Notification message="There is no feedback"></Notification>
   ) : (
-    <ul>
+    <StatisticsList>
       <li>Good: {good}</li>
       <li>Neutral: {neutral}</li>
       <li>Bad: {bad}</li>
       <li>Total: {total}</li>
       <li>Positive feedback: {total === 0 ? 0 : positivePercentage + '%'}</li>
-    </ul>
+    </StatisticsList>
   );
 };
 
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const StatisticsList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
